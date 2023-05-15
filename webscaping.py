decc9f6699382142af83f8ff77475975f2fc1830
kumar833ankit <74098972+kumar833ankit@users.noreply.github.com>
2023-05-15 13:27:32 +0530

    Add files via upload

---
+++ webscaping.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# Making a GET request
+r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+
+# Parsing the HTML
+soup = BeautifulSoup(r.content, 'html.parser')
+
+s = soup.find('div', class_='entry-content')
+
+lines = s.find_all('p')
+
+for line in lines:
+	print(line.text)
